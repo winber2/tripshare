308ce74522a0418e8db2c694654f4a3e1049b0fa
Winber Xu <winber1@gmail.com>
2019-04-05 00:35:17 -0700

    Change entry file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 export default class App extends React.Component {
   render() {
